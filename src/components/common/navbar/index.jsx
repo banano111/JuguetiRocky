73745e95c675d2a94aaf2a0a7a705b53b95e6721
Alan Arturo Pedraza Luna <alanbanano@gmail.com>
2021-11-14 21:44:55 -0600

    First Row of Navbar renewed, it has center elements now. INICIO now its a Link to /

--- src/components/common/navbar/index.jsx
+++ src/components/common/navbar/index.jsx
@@ -10,21 +10,21 @@ const Header = () => {
     return (
         <>
             <div className="container-fluid">
-                <div className="row mb-3">
-                    <div className="col-lg-4 welcome-user">
-                        Bienvenido <p /> Manuel
+                <div className="row align-items-center p-2 mb-2">
+                    <div className="col ms-3">
+                        <b>¡Bienvenido Manuel!</b>
                     </div>
-                    <div className="col-lg-4">
-                        <img className="logo-style" src={Logo} height="80px" width="150px"/>
+                    <div className="col d-flex">
+                        <img alt="Logo JuguetiRocky" className="mx-auto" src={Logo} height="80px" width="150px"/>
                     </div>
-                    <div className="col-lg-4 caja-carrito">
-                        <img className="carrito" src={Carrito}/>
+                    <div className="col text-end">
+                        <img alt="Carrito Compra" src={Carrito}/>
                         1 articulo
                     </div>
                 </div>
             </div>
             <div className="container-fluid border border-danger ">
-                <nav className="navbar navbar-expand-lg navbar-dark mx-auto" id="inferior-navbar" >
+                <nav className="navbar navbar-expand-lg navbar-dark mx-auto" id="inferior-navbar">
                     <div className="container-fluid">
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
@@ -32,11 +32,12 @@ const Header = () => {
                         <div className="collapse navbar-collapse ml-3 " id="navbarNavDarkDropdown">
                             <ul className="navbar-nav ">
                                 <li className="nav-item dropdown ">
-                                    <a className="nav-link" href="#">INICIO</a>
+                                    <Link to="/" className="nav-link">INICIO</Link>
                                 </li>
                                 <li className="nav-item dropdown">
 
-                                    <a className="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <a className="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button"
+                                        data-bs-toggle="dropdown" aria-expanded="false">
                                         CATEGORIAS
                                     </a>
                                     <ul className="dropdown-menu " aria-labelledby="navbarDarkDropdownMenuLink">
@@ -48,7 +49,8 @@ const Header = () => {
                                 </li>
                                 <li className="nav-item dropdown">
 
-                                    <a className="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <a className="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button"
+                                        data-bs-toggle="dropdown" aria-expanded="false">
                                         PRODUCTOS
                                     </a>
                                     <ul className="dropdown-menu" aria-labelledby="navbarDarkDropdownMenuLink">
@@ -64,7 +66,8 @@ const Header = () => {
                                 </li>
                                 <li className="nav-item dropdown">
 
-                                    <a className="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <a className="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button"
+                                        data-bs-toggle="dropdown" aria-expanded="false">
                                         MARCAS
                                     </a>
                                     <ul className="dropdown-menu" aria-labelledby="navbarDarkDropdownMenuLink">
