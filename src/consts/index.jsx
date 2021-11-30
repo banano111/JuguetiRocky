69e4844ddc438b2c0c56fefe35a0727b52293cde
Alan Arturo Pedraza Luna <alanbanano@gmail.com>
2021-11-30 03:52:06 -0600

    Const Move to Prod Stage

--- src/consts/index.jsx
+++ src/consts/index.jsx
@@ -1,2 +1,2 @@
-export const API_URL_PROD = "https://juguetirocky-api.herokuapp.com"
-export const API_URL = "http://localhost:8000"
+export const API_URL = "https://juguetirocky-api.herokuapp.com"
+export const API_URL_DEV = "http://localhost:8000"
